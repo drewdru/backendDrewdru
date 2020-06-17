86a7a87d5d6291b0c47f5ea258abc0120d50c8bc
Drew Dru <drew.drux@gmail.com>
2020-06-17 22:50:19 +0700

    add websockets

--- backendDrewdru/asgi.py
+++ backendDrewdru/asgi.py
@@ -11,6 +11,19 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+from backendDrewdru.websocket import websocket_application
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backendDrewdru.settings_prod")
 
-application = get_asgi_application()
+django_application = get_asgi_application()
+
+
+async def application(scope, receive, send):
+    print(scope["type"])
+    if scope["type"] == "http":
+        await django_application(scope, receive, send)
+    elif scope["type"] == "websocket":
+        await websocket_application(scope, receive, send)
+    else:
+        raise NotImplementedError(f"Unknown scope type {scope['type']}")
+
